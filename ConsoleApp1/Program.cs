790f70e0e214770d4dd1621677d5f7c3c8f13b62
Felix Omondi <felixomosh7@gmail.com>
2024-08-16 17:52:56 +0300

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Net.NetworkInformation;
+
+//this is a comment
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello CSharp");
+
+            //declaring variables
+            int x, y, z;
+            x = y = z = 50;
+            Console.WriteLine(x + y + z);
+            int j = 10, k = 20, l = 30;
+            Console.WriteLine(j + k + l);
+            const double Pi = 3.142;
+            Console.WriteLine(Pi);
+
+            //data types
+            string name = "Syncfusion";
+            Console.WriteLine(name[0]);
+            string vikings = "We're \"Vikings\" from the North";
+            Console.WriteLine(vikings);
+            int age = 23;
+            Console.WriteLine(age);
+            double revenue = 10.2;
+            Console.WriteLine(revenue);
+            char init = 'S';
+            Console.WriteLine(init);
+            bool old = true;
+            Console.WriteLine(old);
+
+            //type casting
+            string num1 = "1";
+            Console.WriteLine(Convert.ToInt32(num1));
+
+            Console.WriteLine("Enter you name ");
+            string userInput = Console.ReadLine().Trim();
+            Console.WriteLine($"Your name is {userInput}");
+
+            //operators/Maths
+            int number1 = 2;
+            int number2 = 3;
+            Console.WriteLine(number1 + number2);
+            Console.WriteLine(number1 * number2);
+            Console.WriteLine(number1 - number2);
+            Console.WriteLine(number2 / number1);
+            number1 += 1;
+            number1++;
+            Console.WriteLine(Math.Max(number1, number2));
+            Console.WriteLine(Math.Sqrt(64));
+            Console.WriteLine(number1 < number2);
+            Console.WriteLine(number1 == number2);
+            Console.WriteLine(number1 != number2);
+
+            Console.WriteLine(number1 > number2 && 3 > 2);
+            Console.WriteLine(number1 > number2 || 4 > 3);
+            Console.WriteLine(!(number1 > number2));
+
+            //booleans
+            bool isMarried = false;
+            bool isYouth = true;
+            Console.WriteLine(isMarried);
+            Console.WriteLine(isYouth);
+            int myAge = 25;
+            int votingAge = 18;
+            if (myAge >= votingAge)
+            {
+                Console.WriteLine("Old enough to vote");
+            }
+            else
+            {
+                Console.WriteLine("Not old enough to vote");
+            }
+            string result = (myAge > votingAge) ? "Old enough to vote" : "Not old enough to vote";
+            Console.WriteLine(result);
+
+            int days = 5;
+            switch (days)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+                default:
+                    Console.WriteLine("what day?");
+                    break;
+            }
+            int i = 0;
+            while (i < 5)
+            {
+                Console.WriteLine("Coding is fun");
+                i++;
+            }
+
+            int m = 0;
+            do
+            {
+                Console.WriteLine("CSharp is cool");
+                m++;
+            } while (m < 5);
+
+            for (int s = 0; s < 5; s++)
+            {
+                Console.WriteLine("I am a software engineer");
+            }
+
+            string[] myCars = { "Mercedes", "volvo", "Audi", "BMW", "Mazda" };
+            foreach (string car in myCars)
+            {
+                Console.WriteLine(car);
+            }
+
+            int[,] my2d = { { 1, 2, 3 }, { 4, 5, 6 } };
+            foreach (int num in my2d)
+            {
+                Console.WriteLine(num);
+            }
+
+            int[] myNumbers = { 2, 1, 4, 7, 8, 9, 3, 5 };
+            Array.Sort(myNumbers);
+            foreach (int num in myNumbers)
+            {
+                Console.WriteLine(num);
+            }
+        }
+
+    }
+}
